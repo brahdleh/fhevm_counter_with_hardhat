b835ccfdd3c7a503f9da2237b552eb0b7c4e7e6b
Bradley <90146750+brahdleh@users.noreply.github.com>
2025-01-20 13:52:25 +0000

    added simple tests for increment

--- contracts/EncryptedCounter.sol
+++ contracts/EncryptedCounter.sol
@@ -9,8 +9,7 @@ import "fhevm/config/ZamaFHEVMConfig.sol";
 /// @notice A basic contract demonstrating the setup of encrypted types
 /// @dev Uses TFHE library for fully homomorphic encryption operations
 /// @custom:experimental This is a minimal example contract intended only for learning purposes
-/// @custom:notice This contract has limited real-world utility and serves primarily as a starting point
-/// for understanding how to implement basic FHE operations in Solidity
+/// @custom:notice This contract has limited real-world utility
 contract EncryptedCounter1 is SepoliaZamaFHEVMConfig {
     euint8 internal counter;
     euint8 internal immutable CONST_ONE;
